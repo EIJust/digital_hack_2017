e62a565692d57a4e84c76d1c33110764a3cab5c4
Isaev Egor <stg.egor.isaev@yandex.ru>
2017-11-04 19:21:46 +0700

    add triger

--- detected_contours.py
+++ detected_contours.py
@@ -2,12 +2,21 @@ import cv2
 import numpy as np
 import imutils
 
+clicked = False
 
 def nothing():
     pass
 
 
-img = cv2.imread('tasks/EM/ImagesEM/Tv2_18000.jpg')
+def triger_fn(cont_list):
+    for conture in cont_list:
+        min_x = contour.min(0)
+        max_x = contour.max(0)
+        min_y = contour.min(1)
+        max_y = contour.max(1)
+
+
+img = cv2.imread('/home/egor/playground/digital_hack_2017/tasks/EM/ImagesEM/BNP9.jpg')
 
 if img.shape[1] > 600:
     img = imutils.resize(img, width=600)
@@ -17,7 +26,9 @@ cv2.namedWindow('Treshed')
 # create trackbars for treshold change
 cv2.createTrackbar('Treshold', 'Treshed', 0, 255, nothing)
 
-while (1):
+prev_tresh_value = 0
+waitable_iterations = 0
+while True:
     clone = img.copy()
 
     gray = cv2.cvtColor(clone, cv2.COLOR_BGR2GRAY)
@@ -47,13 +58,24 @@ while (1):
     cv2.drawContours(clone, contour_list, -1, (255, 0, 0), 2)
 
     # there is an outer boundary and inner boundary for each eadge, so contours double
-    print('Number of found circles: {}'.format(int(len(contour_list) / 2)))
+    # print('Number of found circles: {}'.format(int(len(contour_list) / 2)))
 
     cv2.imshow('Objects Detected', clone)
     cv2.imshow("Treshed", gray_threshed)
 
     k = cv2.waitKey(1) & 0xFF
+    if prev_tresh_value == r:
+        waitable_iterations += 1
+    else:
+        waitable_iterations = 0
+        prev_tresh_value = r
+        clicked = False
+    if waitable_iterations > 150 and not clicked:
+        clicked = True
+        triger_fn(contour_list)
+
     if k == 27:
         break
 
 cv2.destroyAllWindows()
+
