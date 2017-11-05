5e1367ec7bbb6822c9b62ffb39e7d80f9c8d1d10
seregashilyaev <shilyaev.sergey@yandex.ru>
2017-11-05 14:16:35 +0700

    Add simple interface for uploading images

---
+++ gui.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow, QTextEdit, QAction, QFileDialog, QPushButton, \
+    QAction
+from PyQt5.QtGui import QIcon, QPixmap
+
+from granulas_detector import detect_granulas
+
+
+class Gui(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def open_image(self):
+        file_name = QFileDialog.getOpenFileName(self, 'Open Image', '', 'Image Files (*.jpg)')
+        detect_granulas(file_name[0], True)
+
+    def init_ui(self):
+        upload_action = QAction('Upload image', self)
+        upload_action.triggered.connect(self.open_image)
+
+        self.statusBar()
+
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('&File')
+        file_menu.addAction(upload_action)
+
+        self.setGeometry(200, 200, 350, 250)
+        self.setWindowTitle('Main window')
+
+
+if __name__ == '__main__':
+    gui = QApplication(sys.argv)
+
+    mainWindow = Gui()
+    mainWindow.show()
+
+    ex = Gui()
+    sys.exit(app.exec_())
